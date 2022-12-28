135862d26667813bdef9958ec62668508bc10ccb
Wiverson Lima <68648223+wiverson-nogueira@users.noreply.github.com>
2022-12-28 15:50:57 -0300

    Update main.py
    Adicionando a variável de ambiente do SSL_CERT_FILE

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from kivy.lang import Builder
 from telas import *
 from botoes import *
 import requests
+import os 
+import certifi
 from bannervenda import BannerVenda
 import os
 from functools import partial
 from myfirebase import MyFirebase
 from bannervendedor import BannerVendedor
 from datetime import date
+
+os.environ['SSL_CERT_FILE'] = certifi.where()
 
 GUI = Builder.load_file('main.kv')
 class MainApp(App):
